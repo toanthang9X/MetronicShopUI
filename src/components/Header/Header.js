015d89bf9ee3d756e98759d87df549fab2341e37
Thang Tran Toan <ttthang@tma.com.vn>
2022-09-09 11:31:49 +0700

    refactor header code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,30 +4,32 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FiSearch, FiMenu } from "react-icons/fi";
 import { IoCaretUpSharp } from "react-icons/io5";
 import WorkShopLogo from "../../assets/images/logo-shop-red.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
+  const history = useNavigate();
   const [showSearch, setShowSearch] = useState(false);
   const [showNav, setShowNav] = useState(false);
-
   const handleSearch = () => {
     setShowSearch((state) => !state);
   };
   const handleNav = () => {
     setShowNav((state) => !state);
   };
+  const handleToHome = () => {
+    history('/');
+  }
 
   return (
     <div className="header">
       <div className="header__logo">
-        <img src={WorkShopLogo} alt="picture Logo" />
+        <img src={WorkShopLogo} alt="pictureLogo" onClick={handleToHome}/>
       </div>
       <div className="header__menu">
         <button onClick={handleNav}>
-        <FiMenu />
+          <FiMenu />
         </button>
       </div>
-
       <ul className={showNav ? 'header__nav' : 'header__nav-hidden'}>
         <li onClick={handleNav}>
           <Link to="/">Home</Link>
@@ -54,10 +56,11 @@ function Header() {
             </>
             )}
         </div>
-
         <div className="header__cart">
             <button>
-            <Link to="/cart"><AiOutlineShoppingCart className="header__cart-ic" /></Link>
+              <Link to="/cart">
+                <AiOutlineShoppingCart className="header__cart-ic" />
+              </Link>
             </button>
         </div>
       </div>
